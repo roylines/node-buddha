c077410877a85c9785b6c94965ac1fa331380062
Roy Lines <roy@roylines.co.uk>
2012-02-18 13:16:14 +0000

    'added setCredentials'

--- test/buddha_test.js
+++ test/buddha_test.js
@@ -2,12 +2,26 @@ var vows = require('vows'),
     assert = require('assert'),
     buddha = require('../lib/buddha.js');
 
-vows.describe('buddha').addBatch({
-    'when true': {
-        topic: true,
-
-        'should equal true': function(check) {
-            assert.equal(check, true);
+vows.describe('testing buddha test credentials').addBatch({
+    'with valid credentials': {
+        topic: { 
+            email: "a@a.a",
+            password: "PASSWORD" 
+        },
+        'calling setCredentials' : {
+            topic: function(credentials) {
+                buddha.setCredentials(credentials.email, credentials.password);
+                return buddha;
+            },
+            'should set email' : function(b) {                
+                assert.equal(b.email, "a@a.a");
+            },
+            'should set password' : function(b) {                
+                assert.equal(b.password, "PASSWORD");
+            },
+            teardown : function(b) {
+                b.setCredentials(null, null);
+            }            
         }
     }
 }).export(module);
