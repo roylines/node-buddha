74b8e824cee8b18a9534ed0682154c958d399b18
Roy Lines <roy@roylines.co.uk>
2012-02-18 10:20:25 +0000

    'added simple vows test'

---
+++ test/buddha_test.js
@@ -0,0 +1,13 @@
+var vows = require('vows'),
+    assert = require('assert'),
+    buddha = require('../lib/buddha.js');
+
+vows.describe('buddha').addBatch({
+    'when true': {
+        topic: true,
+
+        'should equal true': function(check) {
+            assert.equal(check, true);
+        }
+    }
+}).run();
