102f1e8445a05965b30ee6a2a16d7e6e7f1d7c57
Roy Lines <roy@roylines.co.uk>
2012-02-19 09:07:59 +0000

    'added getUsers'

--- test/buddha_test.js
+++ test/buddha_test.js
@@ -2,40 +2,52 @@ var vows = require('vows'),
     assert = require('assert'),
     buddha = require('../lib/buddha.js');
 
-vows.describe('buddha')
-.addBatch({
-    'with valid credentials': {
-        topic: { 
-            email: "a@a.a",
-            password: "PASSWORD" 
+vows.describe('buddha').addBatch({
+  'with valid credentials': {
+    topic: {
+      email: "a@a.a",
+      password: "PASSWORD"
+    },
+    'calling setCredentials': {
+      topic: function(credentials) {
+        buddha.setCredentials(credentials.email, credentials.password);
+        return buddha;
+      },
+      'should set email': function(b) {
+        assert.equal(b.email, "a@a.a");
+      },
+      'should set password': function(b) {
+        assert.equal(b.password, "PASSWORD");
+      },
+      teardown: function(b) {
+        b.setCredentials(null, null);
+      }
+    },
+    'calling setOnBehalfOf': {
+      topic: function(credentials) {
+        buddha.setOnBehalfOf(credentials.email);
+        return buddha;
+      },
+      'should set onBehalfOf': function(b) {
+        assert.equal(b.onBehalfOf, "a@a.a");
+      },
+      teardown: function(b) {
+        b.setOnBehalfOf(null);
+      }
+    },
+    'after setting the credentials': {
+      topic: function(credentials) {
+        buddha.setCredentials(credentials.email, credentials.password);
+        return buddha;
+      },
+      'and calling getUsers': {
+        topic: function(b) {
+          b.getUsers(this.callback);
         },
-        'calling setCredentials' : {
-            topic: function(credentials) {
-                buddha.setCredentials(credentials.email, credentials.password);
-                return buddha;
-            },
-            'should set email' : function(b) {                
-                assert.equal(b.email, "a@a.a");
-            },
-            'should set password' : function(b) {                
-                assert.equal(b.password, "PASSWORD");
-            },
-            teardown : function(b) {
-                b.setCredentials(null, null);
-            }            
-        },
-        'calling setOnBehalfOf' : {
-            topic: function(credentials) {
-                buddha.setOnBehalfOf(credentials.email);
-                return buddha;
-            },
-            'should set onBehalfOf' : function(b) {                
-                assert.equal(b.onBehalfOf, "a@a.a");
-            },
-            teardown : function(b) {
-                b.setOnBehalfOf(null);
-            }            
+        'should not error': function(error, data) {
+          assert.isNull(error);
         }
-
+      }
     }
+  }
 }).export(module);
